072d6b694d0e68c757e17a50c7ef1b157fcbd931
krakodjaba <documentheightafter@gmail.com>
2026-06-08 08:18:25 +0300

    fix sources

--- proxytool/sources.py
+++ proxytool/sources.py
@@ -5,14 +5,12 @@ import re
 IP_PORT_RE = re.compile(r"(\d{1,3}(?:\.\d{1,3}){3}):(\d{2,5})")
 
 HTTP_SOURCES: list[str] = [
-    "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http"
-    "&timeout=10000&country=all&ssl=all&anonymity=all",
+    "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http",
     "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
 ]
 
 SOCKS5_SOURCES: list[str] = [
-    "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks5"
-    "&timeout=10000&country=all",
+    "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks5",
     "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txt",
     "https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt",
 ]
