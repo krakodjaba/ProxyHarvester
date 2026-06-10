5d1ad2e7acd8bbcd68fdfe3839accdb5e857cd4a
Eduard Isaev <107117398+krakodjaba@users.noreply.github.com>
2026-06-11 00:58:37 +0300

    Update sources.py

--- proxytool/sources.py
+++ proxytool/sources.py
@@ -7,6 +7,9 @@ IP_PORT_RE = re.compile(r"(\d{1,3}(?:\.\d{1,3}){3}):(\d{2,5})")
 HTTP_SOURCES: list[str] = [
     "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http",
     "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
+    "https://raw.githubusercontent.com/proxifly/free-proxy-list/refs/heads/main/proxies/all/data.txt",
+    "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/refs/heads/master/http.txt".
+    "https://advanced.name/freeproxy/6a29dcccb0aa6?type=http?type=https?type=anon?type=elite"
 ]
 
 SOCKS5_SOURCES: list[str] = [
